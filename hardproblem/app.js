82ea446e7325ff6be9284a23ba31b0010ce99726
pasberth <pasberth@gmail.com>
2014-08-01 13:58:48 +0900

    目次

--- hardproblem/app.js
+++ hardproblem/app.js
@@ -21,7 +21,6 @@ Vue.component('hpot_header', {
           + '      <a href="#{{section.id}}">{{section.title}}</a>'
           + '    </li>'
           + '  </ol>'
-
           + '  <div class="hr"></div>'
           + '</div>'
 });
@@ -29,12 +28,16 @@ Vue.component('hpot_header', {
 Vue.component('hpot_footer', {
   template: '<div class="footer hpot-footer">'
           + '  <div class="hr"></div>'
-          + '  <ol class="nav">'
-          + '    <li class="nohover">'
-          + '      {{title}}'
-          + '    </li>'
-          + '    <li v-repeat="section : sections" now="{{id === section.id}}">'
-          + '      <a href="#{{section.id}}">{{section.title}}</a>'
+          + '  <ol class="nav1">'
+          + '    <li v-repeat="chapter : chapters">'
+          + '      <ol class="nav">'
+          + '        <li class="nohover">'
+          + '          {{chapter.sectPrefix}} {{chapter.title}}'
+          + '        </li>'
+          + '        <li v-repeat="section : chapter.sections" now="{{id === section.id}}">'
+          + '          <a href="#{{section.id}}">{{section.title}}</a>'
+          + '        </li>'
+          + '      </ol>'
           + '    </li>'
           + '  </ol>'
           + '</div>'
@@ -69,22 +72,120 @@ var chapters = [
                         title: "5",
                         id: "1-05",
                       },
+                      {
+                        title: "6",
+                        id: "1-06",
+                      },
+                      {
+                        title: "7",
+                        id: "1-07",
+                      },
+                      {
+                        title: "8",
+                        id: "1-08",
+                      },
                     ],
                 },
                 {
                   sectPrefix: "§2",
+                  title: "海へ行こう",
+                  sections:
+                    [
+                      {
+                        title: "1",
+                        id: "2-01",
+                      },
+                      {
+                        title: "2",
+                        id: "2-02",
+                      },
+                      {
+                        title: "3",
+                        id: "2-03",
+                      },
+                      {
+                        title: "4",
+                        id: "2-04",
+                      },
+                      {
+                        title: "5",
+                        id: "2-05",
+                      },
+                      {
+                        title: "6",
+                        id: "2-06",
+                      },
+                    ],
                 },
                 {
                   sectPrefix: "§3",
+                  title: "その柔肌は誰のために",
+                  sections:
+                    [
+                      {
+                        title: "1",
+                        id: "3-01",
+                      },
+                      {
+                        title: "2",
+                        id: "3-02",
+                      },
+                      {
+                        title: "3",
+                        id: "3-03",
+                      },
+                      {
+                        title: "4",
+                        id: "3-04",
+                      },
+                      {
+                        title: "5",
+                        id: "3-05",
+                      },
+                      {
+                        title: "6",
+                        id: "3-06",
+                      },
+                      {
+                        title: "7",
+                        id: "3-07",
+                      },
+                    ],
                 },
                 {
                   sectPrefix: "§4",
+                  title: "ことしはひとりじゃない",
+                  sections:
+                    [
+                      {
+                        title: "1",
+                        id: "4-01",
+                      },
+                      {
+                        title: "2",
+                        id: "4-02",
+                      },
+                      {
+                        title: "3",
+                        id: "4-03",
+                      },
+                      {
+                        title: "4",
+                        id: "4-04",
+                      },
+                      {
+                        title: "5",
+                        id: "4-05",
+                      },
+                    ],
                 },
                 {
                   sectPrefix: "§5",
+                  title: "(タイトル未定)",
                 },
                 {
                   sectPrefix: "§6",
+                  title: "窓の向こう(仮)",
                 },
               ];
 
