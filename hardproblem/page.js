34380b3bfe71f39952a4160e56984b71621a925b
pasberth <pasberth@gmail.com>
2014-08-01 20:54:51 +0900

    a

--- hardproblem/page.js
+++ hardproblem/page.js
@@ -3,7 +3,7 @@ var page = new Vue({
   el: '#page',
   data: {
     now: now,
-    chapter: (HPOTCFG.chapters || []).filter(function (chapter) { return chapter.id === now })[0],
+    chapter: (HPOTCFG.chapters || []).filter(function (chapter) { return (chapter.sections || []).filter(function (section) { return section.id === now }).length > 0; })[0],
     chapters: (HPOTCFG.chapters || []),
   }
 });
