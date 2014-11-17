502c4fd8b4c7112287c0d7eee5cdf6e49d0076b1
pasberth <pasberth@gmail.com>
2014-11-17 16:08:03 +0900

    フォント選択

--- hardproblem/components.js
+++ hardproblem/components.js
@@ -13,7 +13,7 @@ Vue.component('hpot-main-header', {
 Vue.component('hpot-page-header', {
   className: 'header hpot-header hpot-page-header',
   template: '<a href="../hardproblem"><img src="../img/hardproblem_logo.png" /></a>'
-          + '<div v-component="hpot-section-index"></div>'
+          + '<div v-component="hpot-section-index-in-page-header"></div>'
           + '<div class="hr"></div>'
 });
 
@@ -43,4 +43,27 @@ Vue.component('hpot-section-index', {
           + '    <a href="{{section.id}}.html">{{section.title}}</a>'
           + '  </li>'
           + '</div>'
+});
+
+Vue.component('hpot-section-index-in-page-header', {
+  className: "hpot-section-index",
+  template: '<p class="title">'
+          + '  <span class="sect-prefix">{{chapter.sectPrefix}}</span>'
+          + '  <span class="title-text">{{chapter.title}}</span>'
+          + '</p>'
+          + '<ol>'
+          + '  <li v-repeat="section : chapter.sections" now="{{now === section.id}}">'
+          + '    <a href="{{section.id}}.html">{{section.title}}</a>'
+          + '  </li>'
+          + '</ol>'
+          + '<br />'
+          + '<select onchange="document.getElementById(\'fontsets\').href = value;" class="fontselect">'
+          + '<option value="../fonts/meiryo.css">メイリオ + Microsoft YaHei</option>'
+          + '<option value="../fonts/notosans.css">Noto Sans CJK</option>'
+          + '<option value="../fonts/yu_mincho.css">游明朝 + AR PL SungtiL GB</option>'
+          + '<option value="../fonts/hanazono_mincho.css">花園明朝</option>'
+          + '<option value="../fonts/sans-serif.css">MSゴシック／ヒラギノ角ゴ</option>'
+          + '<option value="../fonts/serif.css">MS明朝／ヒラギノ明朝</option>'
+          + '<option>ブラウザ指定</option>'
+          + '</select>'
 });
