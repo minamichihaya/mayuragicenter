d9b8591a97f0e78b70dfa0645ece6f4063c3dd47
Minami Xōtarō <ns850813@gmail.com>
2015-09-23 02:25:35 +0900

    文言の修正

--- metastable3/cookie.js
+++ metastable3/cookie.js
@@ -1,6 +1,6 @@
 string=document.cookie;
-flag3=string.indexOf("metastableflag3");
-flag7=string.indexOf("metastableflag7");
+flag3=string.indexOf("ms3flag3");
+flag7=string.indexOf("ms3flag7");
 if(flag7 == -1){
   if(flag3 == -1){
     document.getElementById("cover").src="../img/ms3_title0.png";
