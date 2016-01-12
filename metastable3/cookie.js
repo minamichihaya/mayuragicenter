823b9245178df903705b710f749ce9e3e4a051cd
Minami Xōtarō <ns850813@gmail.com>
2016-01-12 23:45:14 +0900

    メタステ§1に絵をつける

--- metastable3/cookie.js
+++ metastable3/cookie.js
@@ -3,7 +3,7 @@ flag3=string.indexOf("ms3flag3");
 flag7=string.indexOf("ms3flag7");
 if(flag7 == -1){
   if(flag3 == -1){
-    document.getElementById("cover").src="../img/ms3_title0.png";
+    document.getElementById("cover").src="../img/ms3_title1.png";
   }else{
     document.getElementById("cover").src="../img/ms3_title0.png";
   }
