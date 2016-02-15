6eadba6eee10e11f677d200379ffc49a68d26fd9
Minami Xōtarō <ns850813@gmail.com>
2016-02-15 21:17:04 +0900

    フラグ番号変更

--- hardproblem/cookie.js
+++ hardproblem/cookie.js
@@ -1,8 +1,8 @@
 string=document.cookie;
-flag3=string.indexOf("hpftflag3");
+flag4=string.indexOf("hpftflag3");
 flag7=string.indexOf("hpftflag7");
 if(flag7 == -1){
-  if(flag3 == -1){
+  if(flag4 == -1){
     document.getElementById("cover").src="../img/title1.png";
   }else{
     document.getElementById("cover").src="../img/title2.png";
