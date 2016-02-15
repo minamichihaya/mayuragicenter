1fd6235221ef70ee1ddfb4bd6690af57a89b8314
Minami Xōtarō <ns850813@gmail.com>
2016-02-15 21:17:16 +0900

    用地買収

--- metastable3/cookie.js
+++ metastable3/cookie.js
@@ -1,12 +1,12 @@
 string=document.cookie;
-flag3=string.indexOf("ms3flag3");
+flag4=string.indexOf("ms3flag3");
 flag7=string.indexOf("ms3flag7");
 if(flag7 == -1){
-  if(flag3 == -1){
+  if(flag4 == -1){
     document.getElementById("cover").src="../img/ms3_title1.png";
   }else{
-    document.getElementById("cover").src="../img/ms3_title0.png";
+    document.getElementById("cover").src="../img/ms3_title2.png";
   }
 }else{
-    document.getElementById("cover").src="../img/ms3_title0.png";
+    document.getElementById("cover").src="../img/ms3_title3.png";
 }
