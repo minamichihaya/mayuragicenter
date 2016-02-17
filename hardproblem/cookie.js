974cdb4a4ea646b1e17c1efee4739803318edfbc
Minami Xōtarō <ns850813@gmail.com>
2016-02-18 00:35:15 +0900

    Revert "スマホ版にタイトル画面出すテスト"
    This reverts commit 052171cc23e6b54c690e299af0085557a2ad4fd6.

--- hardproblem/cookie.js
+++ hardproblem/cookie.js
@@ -4,12 +4,9 @@ flag7=string.indexOf("hpftflag7");
 if(flag7 == -1){
   if(flag4 == -1){
     document.getElementById("cover").src="../img/title1.png";
-    document.getElementById("p-cover").src="../img/title1.png";
   }else{
     document.getElementById("cover").src="../img/title2.png";
-    document.getElementById("p-cover").src="../img/title2.png";
   }
 }else{
     document.getElementById("cover").src="../img/title3.png";
-    document.getElementById("p-cover").src="../img/title3.png";
 }
