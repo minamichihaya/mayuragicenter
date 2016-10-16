3a3a5e64910b5aeddc022784930c2d81d971828f
Minami Xōtarō <ns850813@gmail.com>
2016-10-16 09:23:41 +0900

    ハドプレサイトリニューアルテスト版（作成途中）

---
+++ hardproblem_test/script.js
@@ -0,0 +1,39 @@
+function OpenIndex(){
+    var target = document.getElementById("Index");
+    target.style.display="block";    
+};
+
+function CloseIndex(){
+    var target = document.getElementById("Index");
+    target.style.display="none";
+};
+
+function OpenCharacters(){
+    var target = document.getElementById("Characters");
+    target.style.display="block";    
+};
+
+function CloseCharacters(){
+    var target = document.getElementById("Characters");
+    target.style.display="none";
+};
+
+function OpenKeyWords(){
+    var target = document.getElementById("KeyWords");
+    target.style.display="block"; 
+};
+
+function CloseKeyWords(){
+    var target = document.getElementById("KeyWords");
+    target.style.display="none";
+};
+
+function OpenSetting(){
+    var target = document.getElementById("Setting");
+    target.style.display="block"; 
+};
+
+function CloseSetting(){
+    var target = document.getElementById("Setting");
+    target.style.display="none";
+};
