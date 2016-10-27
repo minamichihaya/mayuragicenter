1c175cd6bb99c45283900112992ac13fb815d8fe
Minami Xōtarō <ns850813@gmail.com>
2016-10-28 03:08:39 +0900

    進捗

--- hardproblem_test/script.js
+++ hardproblem_test/script.js
@@ -70,9 +70,16 @@ function DisplayCover(){
     document.getElementById("Cover").style.display = "block";
 }
 
-function ChangeLoading() {
-    document.getElementById("Loading").style.backgroundImage = 'url("images/' + Phase + '/loading.gif")';
+
+function DisplayContent() {
+    document.getElementById("LoadingIcon").style.display = "none";
+    document.getElementById("TextBackground").style.display = "block";
 }
+
+function ChangeLoading() {
+    document.getElementById("LoadingIcon").src = "images/" + Phase + "/loading.gif";
+}
+
 
 
     
@@ -90,6 +97,42 @@ function MakeCover(){
     target[0].innerHTML = target[0].innerHTML.replace(/images\/[1-3]\//g,"images/" + Phase + "/");
     target = document.getElementById("Cover");
     target.style.backgroundImage = 'url("images/' + Phase + '/cover.png")';
+    target = document.getElementById("Index1");
+    target.innerHTML = '<span class="SecSpan"><a href="' + SecList[0].section + '.html">' + SecList[0].title + '</a></span>';
+    target = document.getElementById("SectionTitle");
+    for (var i=0; i<=SecList.length-1; i++) {
+        if (SecList[i].subsection != "0") {
+            target.innerHTML = target.innerHTML + '§' + SecList[i].section + ' '　+ SecList[i].title;
+            if (SecList[i+1].subsection != "0") {
+                target.innerHTML = target.innerHTML + '<br>';
+            }
+        }
+    }
+    target = document.getElementById("Subsection");
+    for (var i=0; i<=SecList.length-1; i++) {
+        if (SecList[i].subsection !="0") {
+            for (var j=0; j<=SecList[i].subsection.length-1; j++) {
+                target.innerHTML = target.innerHTML
+                                    + '<span class="SubsecSpan" id="' + SecList[i].section + '-' + SecList[i].subsection[j]
+                                    + '"><a href="' + SecList[i].section + '-' + SecList[i].subsection[j]
+                                    + '.html">' + SecList[i].subsection[j] + '</a></span>';
+            }
+            if (SecList[i+1].subsection != "0") {
+                target.innerHTML = target.innerHTML + '<br>';
+            }
+        }
+    }
+    target = document.getElementById("Index3");
+    for (var i=1; i<=SecList.length-1; i++) {
+        if (SecList[i].subsection=="0") {          
+            target.innerHTML = target.innerHTML
+                                + '<span class="SecSpan"><a href="' + SecList[i].section + '.html">'
+                                + SecList[i].title + '</a></span>';
+            if (i!=SecList.length-1) {
+                target.innerHTML = target.innerHTML + '<br>';
+            }
+        }
+    }
 }
 
 function OpenIndex(){
@@ -157,6 +200,7 @@ function MakeContent() {
     var CurrentSubsection;
     var HTML = new Array();
     var Content;
+    var target;
     
 
     /********************************************************************
@@ -176,32 +220,60 @@ function MakeContent() {
     SecTitle = GetSecData(Section,"section","title");
     
     /********************************************************************
-        head/bodyの出力"0"
+        head/bodyの出力
     *********************************************************************/
     
 
     Text.open("GET", "texts/html.txt", false);
     Text.send("");
-
+    target = document.getElementsByTagName("body");
+    
+    target[0].outerHTML = Text.responseText;
+    target = document.getElementsByTagName("title");
     if (Subsection == "0") {
-        HeadAndBody = Text.responseText.replace("<title>???</title>",
-                                                "<title>" + SecTitle + "／" + NovelTitle + "</title>");
+        target[0].innerHTML = SecTitle + "／" + target.innerHTML;
     } else {
-        HeadAndBody = Text.responseText.replace("<title>???</title>",
-                                                "<title>§" + Section + " " + SecTitle + "(" + Subsection + "／" + NovelTitle + "</title>");
-    };
+        target[0].innerHTML = "§" + Section + " " + SecTitle + "(" + Subsection + ")／" + target[0].innerHTML;
+    }
+    target = document.getElementsByTagName("link");
+    target[0].outerHTML = target[0].outerHTML.replace('<link rel="shortcut icon" href="images/1/favicon.png" type="image/png">',
+                                                      '<link rel="shortcut icon" href="images/'+ Phase + '/favicon.png" type="image/png">');
+    target = document.getElementById("LoadingIcon");
+    target.src = 'images/' + Phase + '/loading.gif';
+    if (Subsection == "1") {
+        target = document.getElementById("SecCoverImg");
+        target.src = 'images/section' + Section + '.png';
+        target = document.getElementById("SecCoverLogo");
+        target.src = 'images/' + Phase + '/logo_sl.png';
+        switch (Section) {
+            case "1":
+                target.style.right = "5px"
+                break;
+            case "2":
+                target.style.left = "5px"
+                break;
+            case "3":
+                target.style.left = "5px"
+                break;
+            case "4":
+                target.style.right = "5px"
+                break;
+            case "5":
+                target.style.left = "5px"
+                break;
+            case "6":
+                target.style.right = "5px"
+                break;
+        }
+        
+    } else {
+        target = document.getElementById("SectionCover");
+        target.style.display = "none"
+    }
+    target = document.getElementById("SecCoverLogo");
+    
 
-    HeadAndBody = HeadAndBody.replace("images/???/favicon.png","images/" + Phase + "/favicon.png");
-
-    if (Subsection == "1"){
-        HeadAndBody = HeadAndBody.replace("images/section???.png","images/section" + Section + ".png");
-    }else{
-        HeadAndBody = HeadAndBody.replace('<img src="images/section???.png" id="SectionCover">','');
-    };
-
-
-    document.getElementsByTagName("html")[0].innerHTML = HeadAndBody;
-
+    
     /********************************************************************
         ヘッダー／フッターのリンク修正
     *********************************************************************/
@@ -221,24 +293,32 @@ function MakeContent() {
             document.getElementById("FooterNext").style.visibility="hidden";
         } else {
             if (Subsection == "0") {
-                if (Section == "epilogue") {
-                    Prev[i].href = '6-5.html';
-                    Next[i].href = 'afterwords.html';
-                } else {
-                    Prev[i].href = SecList[GetSecData(Section,"section","index") - 1]["section"] + ".html";
-                    Next[i].href = SecList[GetSecData(Section,"section","index") + 1]["section"] + ".html";
+                switch (Section) {
+                    case "epilogue":
+                        Prev[i].href = '6-5.html';
+                        Next[i].href = 'afterwords.html';
+                        break;
+                    case "afterwords":
+                        Prev[i].href = 'epilogue.html';
+                        Next[i].href = 'acknowledgements.html';
+                        break;
+                    case "acknowledgements":
+                        Prev[i].href = 'afterwords.html';
+                        Next[i].href = 'references.html';
+                        break;
                 }
-            } else if (Subsection == "1") {
+            } else {
+                if (Subsection == "1") {
                     if (Section == "1") {
                         Prev[i].href = 'prologue.html';
                         Next[i].href = '1-2.html';
                     } else {
                         Prev[i].href = String(Number(Section) - 1) + "-"
-                                        + String(SecList[GetSecData(Section,"section","index")]["subsection"].length + 1) 
+                                        + String(SecList[GetSecData(String(Number(Section)-1),"section","index")]["subsection"].length) 
                                         + '.html';
                         Next[i].href = Section + "-" + String(Number(Subsection) + 1) + '.html';
                     }
-            } else if (Subsection == SecList[GetSecData(Section,"section","index")]["subsection"].length + 1) {
+                } else if (Subsection == String(SecList[GetSecData(Section,"section","index")]["subsection"].length)) {
                     if (Section == "6") {
                         Prev[i].href = '6-4.html';
                         Next[i].href = 'epilogue.html';
@@ -246,17 +326,28 @@ function MakeContent() {
                         Prev[i].href = Section + "-" + String(Number(Subsection) - 1) + '.html';
                         Next[i].href = String(Number(Section) + 1) + "-" + '1.html';
                     }
-            } else {
-                Prev[i].href = Section + "-" + String(Number(Subsection) - 1) + '.html';
-                Next[i].href = Section + "-" + String(Number(Subsection) + 1) + '.html';
+                } else {
+                    Prev[i].href = Section + "-" + String(Number(Subsection) - 1) + '.html';
+                    Next[i].href = Section + "-" + String(Number(Subsection) + 1) + '.html';
+                }
             }
         }
     }
- 
+                
+                
+    /********************************************************************
+        CSSの読み込み
+    *********************************************************************/
 
+
+    HTML = document.getElementsByTagName("html");
+    HTML[0].innerHTML = HTML[0].innerHTML + '<link rel="stylesheet" href="style.css" type="text/css">';
+    
+    
     /********************************************************************
         本文の表示
     *********************************************************************/
+
 
     if (Subsection !== "0") {
         Text.open("GET", "texts/" + Section + "-" + Subsection + ".txt", false);
@@ -266,7 +357,7 @@ function MakeContent() {
 
     Text.send("");
 
-    Content = Text.responseText;
+    var Content = Text.responseText;
 
     while (Content.indexOf('|') > -1) {
         Content = Content.replace('|','<ruby>');
@@ -280,15 +371,7 @@ function MakeContent() {
     };
 
     document.getElementById("Content").innerHTML = Content;
-
-
-
-    /********************************************************************
-        CSSの読み込み
-    *********************************************************************/
-
-
-    HTML = document.getElementsByTagName("html");
-    HTML[0].innerHTML = HTML[0].innerHTML + '<link rel="stylesheet" href="style.css" type="text/css">';
-
+    
+    
 }
+
