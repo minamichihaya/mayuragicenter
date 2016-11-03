71425a91662063d868c8c9b4f7b4bfee3cd9fbd6
Minami Xōtarō <ns850813@gmail.com>
2016-11-03 21:30:22 +0900

    ようやくEdge/IEで動いたっぽよ。・・

--- hardproblem/script.js
+++ hardproblem/script.js
@@ -595,17 +595,13 @@ function MakeContent() {
         document.getElementById("Footer").style.color = "#333";
     }
     
-    window.onload = DisplayContent();
-    console.log("背景色変更\r" + document.getElementsByTagName("body")[0].outerHTML);
-    
+    window.onload = DisplayContent();    
 }
 
 /********************************************************************
         本文の表示
 *********************************************************************/
 function DisplayContent() {
-    
-    alert("displaycontent");
 
     var FileName = window.location.href.split("/").pop();
 
