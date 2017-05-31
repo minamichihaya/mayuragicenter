178da4349e61c48312cd901e67d96f7edb45a20f
mayuragicenter <mayuragint@gmail.com>
2017-06-01 05:22:35 +0900

    ちょい変更

--- tracking.js
+++ tracking.js
@@ -2,14 +2,8 @@
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-if (
-	document.referrer.indexOf("mayuragicenter.github.io") == -1
-	&& document.referrer != ""
-){
-	ga('set', 'dimension9', document.referrer);
-	ga('create', 'UA-90831900-1', 'auto');
-	ga('send', 'pageview');
-}
+ga('create', 'UA-100279260-1', 'auto');
+ga('send', 'pageview');
 setTimeout(function(){
 	if (location.href.indexOf("/hardproblem") != -1) {
 		location.href = "https://mayuragicenter.github.io/hardproblem/";
