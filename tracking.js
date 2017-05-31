c72fb03cd7a30432006fcc426b52dc7aa7426d27
Minami Xōtarō <minamichihaya@users.noreply.github.com>
2017-06-01 04:43:02 +0900

    リファラ調査

---
+++ tracking.js
@@ -0,0 +1,13 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('set', 'dimension7', function(){
+if (
+  document.referrer.indexOf("mayuragicenter.github.io") == -1
+  && document.referrer != ""
+){
+  ga('set', 'dimension9', document.referrer);
+}
+ga('create', 'UA-90831900-1', 'auto');
+ga('send', 'pageview');
